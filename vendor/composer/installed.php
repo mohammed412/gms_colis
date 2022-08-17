d2d4f1d93aedeaea8582eaf8a816b68695b8c1b8
Mohammed412 <mohamedelalami412@gmail.com>
2022-08-17 16:48:30 +0100

    update gitignor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'user1/gms-colis',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ce95fb442492f0012338546888a9ca7c33300d79',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'user1/gms-colis' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ce95fb442492f0012338546888a9ca7c33300d79',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
